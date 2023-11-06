2e1c395b8af90c265d5d11ce559383c41c476276
unknown <you@example.com>
2023-11-06 17:45:54 -0300

    adjust

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 //! Función con efectos secundarios
 
-let outsider = 4
+let outsiderShared = "Eeee";
 
-function sum (num) {
-   return num + outsider
+function sum(num) {
+  console.log(outsiderShared);
+  return num + outsiderShared;
 }
 
-console.log(sum(10))
+console.log(sum(10));
+
+function confioEnOutsideShared() {
+  return outsiderShared;
+}
+
+console.log(confioEnOutsideShared());
 
 //! Función sin efectos secundarios
 
-function sumGenial (num1, num2) {
-   return num1 + num2
+function sumGenial(num1, num2) {
+  return num1 + num2;
 }
 
-console.log(sumGenial(10, 24))
+console.log(sumGenial(1, 1));
